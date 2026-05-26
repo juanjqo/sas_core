4693a6444302ef63dec4bf021930520eb4a789b5
Murilo Marinho <murilo.marinho@manchester.ac.uk>
2026-05-26 20:11:51 +0100

    [sas_object] Update to 2026.

--- src/sas_object.cpp
+++ src/sas_object.cpp
@@ -44,7 +44,7 @@ void Object::_print_license_header(const std::string& class_name)
     std::cout << blue_text << std::string(class_name.size(),'*') 
         << "****************************************************************" << default_color << std::endl;
     std::cout << blue_text << class_name 
-        + " (c) Murilo M. Marinho (murilomarinho.info) 2016-2023 LGPLv3" << default_color << std::endl;
+        + " (c) Murilo M. Marinho (murilomarinho.info) 2016-2026 LGPLv3" << default_color << std::endl;
     std::cout << blue_text << std::string(class_name.size(),'*') 
         << "****************************************************************" << default_color << std::endl;
 }
