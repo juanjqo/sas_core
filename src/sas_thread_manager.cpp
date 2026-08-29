1f6d1c1f8da00735ec57403c51b7c9e288f209bd
Juancho <juanjqogm@gmail.com>
2026-08-29 15:40:14 +0100

    Added missing copyright message

--- src/sas_thread_manager.cpp
+++ src/sas_thread_manager.cpp
@@ -1,3 +1,27 @@
+/*
+# Copyright (c) 2022-2026 Murilo Marques Marinho
+#
+#    This file is part of sas_core.
+#
+#    sas_core is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Lesser General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    sas_core is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Lesser General Public License for more details.
+#
+#    You should have received a copy of the GNU Lesser General Public License
+#    along with sas_core.  If not, see <https://www.gnu.org/licenses/>.
+#
+# ################################################################
+#
+#   Author: Juan Jose Quiroz Omana, email: juanjose.quirozomana@manchester.ac.uk
+#
+# ################################################################*/
+
 #include <sas_core/sas_thread_manager.hpp>
 #include <stdexcept>
 #include <cstring>
